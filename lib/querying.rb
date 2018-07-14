d252e42caada27b69ca192312e2a4b9ae87e934d
Brent Bauman <brentbauman86@gmail.com>
2018-07-14 18:53:22 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -11,7 +11,8 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name AS author_name, subgenres.name AS sub-genre_name FROM series "
+  "SELECT authors.name AS author_name, subgenres.name AS sub-genre_name FROM series INNER JOIN authors
+  ON series.author_id = authors.id INNER JOIN sub-genres ON series.subgenre_id = subgenres.id"
 end
 
 def select_series_title_with_most_human_characters
