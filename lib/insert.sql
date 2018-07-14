b3aa80534eb3b08c8a313e9000f57e86b0c132e4
Brent Bauman <brentbauman86@gmail.com>
2018-07-14 16:56:01 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,13 +1,13 @@
-INSERT INTO series (id, title, author_id, subgenre_id) VALUES 
-(1, "A Song of Ice and Fire", 1, 1), 
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES
+(1, "A Song of Ice and Fire", 1, 1),
 (2, "Second Series", 2, 2);
 
-INSERT INTO subgenres (id, name) VALUES 
-(1, "medieval"), 
+INSERT INTO subgenres (id, name) VALUES
+(1, "medieval"),
 (2, "space opera");
 
-INSERT INTO authors (id, name) VALUES 
-(1, "George R. R. Martin"), 
+INSERT INTO authors (id, name) VALUES
+(1, "George R. R. Martin"),
 (2, "Second Author");
 
 INSERT INTO books (id, title, year, series_id) VALUES
@@ -30,22 +30,22 @@ INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES
 (7, "Character Three", "motto three", "cylon", 2, 2),
 (8, "Character Four", "motto four", "cylon", 2, 2);
 
-INSERT INTO character_books (id, book_id, character_id) VALUES 
-(1, 1, 1), 
-(2, 1, 2), 
-(3, 2, 2), 
-(4, 3, 2), 
-(5, 1, 3), 
-(6, 2, 3), 
-(7, 3, 3), 
+INSERT INTO character_books (id, book_id, character_id) VALUES
+(1, 1, 1),
+(2, 1, 2),
+(3, 2, 2),
+(4, 3, 2),
+(5, 1, 3),
+(6, 2, 3),
+(7, 3, 3),
 (8, 1, 4);
 
-INSERT INTO character_books (id, book_id, character_id) VALUES 
-(9, 4, 5), 
-(10, 4, 6), 
-(11, 5, 6), 
-(12, 6, 6), 
-(13, 4, 7), 
-(14, 5, 7), 
-(15, 6, 7), 
+INSERT INTO character_books (id, book_id, character_id) VALUES
+(9, 4, 5),
+(10, 4, 6),
+(11, 5, 6),
+(12, 6, 6),
+(13, 4, 7),
+(14, 5, 7),
+(15, 6, 7),
 (16, 4, 8);
