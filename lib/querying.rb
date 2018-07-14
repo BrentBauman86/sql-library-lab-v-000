33512d36877391b263890b004059c2a24443907c
Brent Bauman <brentbauman86@gmail.com>
2018-07-14 17:15:41 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,7 +15,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series FROM series WHERE series.id = 1 LIMIT 1"
+  "SELECT title FROM series WHERE series.id = 1 LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
